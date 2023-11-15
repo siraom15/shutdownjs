2b06c0ec3be4b2f632fbd0b76bf06ac85f3fbc1b
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-11-16 03:21:22 +0700

    Refactor

--- index.js
+++ index.js
@@ -5,42 +5,67 @@ import prompt from "prompt";
 import keypress from "keypress";
 import chalk from "chalk";
 
-// Enable keypress events for the process
 keypress(process.stdin);
 
-// Define the shutdown function
 const shutdown = (time) => {
   const command =
     process.platform === "win32"
       ? `shutdown /s /t ${time}`
       : `shutdown -h ${time}`;
 
+  const shutdownTime = new Date(Date.now() + time * 1000);
+  const formattedShutdownTime = shutdownTime.toLocaleTimeString();
+
   exec(command, (error, stdout, stderr) => {
     if (error) {
-      console.error(chalk.red(`Error executing shutdown command: ${error}`));
+      handleError(error, stderr, time);
     } else {
-      console.log(chalk.green("Shutdown scheduled successfully!"));
+      console.log(
+        chalk.green(
+          `Shutdown scheduled successfully!\nYour PC will shutdown at ${formattedShutdownTime}.`
+        )
+      );
+      process.exit(0);
     }
-    process.exit(0);
   });
+};
+
+const handleError = (error, stderr, time = 0) => {
+  if (stderr.includes("A system shutdown has already been scheduled")) {
+    console.log(chalk.yellow("A shutdown is already scheduled. Replacing..."));
+    cancelShutdownSchedule();
+    shutdown(time);
+  } else if (
+    stderr.includes(
+      "Unable to abort the system shutdown because no shutdown was in progress"
+    )
+  ) {
+    console.error(
+      chalk.red(
+        "Error: No shutdown is currently in progress. Please schedule a shutdown first."
+      )
+    );
+  } else {
+    console.error(chalk.red(`Error executing shutdown command: ${error}`));
+    process.exit(1);
+  }
 };
 
 const cancelShutdownSchedule = () => {
   const command = process.platform === "win32" ? `shutdown /a` : `shutdown -a`;
   exec(command, (error, stdout, stderr) => {
     if (error) {
-      console.error(
-        chalk.red(`Error executing Cancel Shutdown Schedule: ${error}`)
-      );
+      handleError(error, stderr);
+      return false;
     } else {
-      console.log(chalk.green("Canceled Shutdown scheduled!"));
+      console.log(chalk.green("Previous shutdown schedule canceled!"));
+      return true;
     }
-    process.exit(0);
   });
 };
 
-// Prompt user for shutdown time
 const menuOptions = [
+  { label: "Shutdown in 15 minutes", time: 15 * 60 },
   { label: "Shutdown in 30 minutes", time: 30 * 60 },
   { label: "Shutdown in 1 hour", time: 60 * 60 },
   { label: "Shutdown in 2 hours", time: 2 * 60 * 60 },
@@ -55,44 +80,33 @@ const displayMenu = () => {
   console.clear();
   console.log(chalk.cyan("Select shutdown time:"));
   menuOptions.forEach((option, index) => {
-    if (index === selectedOptionIndex) {
-      console.log(chalk.cyan(`> ${option.label}`));
-    } else {
-      console.log(chalk.white(`  ${option.label}`));
-    }
+    const label =
+      index === selectedOptionIndex ? `> ${option.label}` : `  ${option.label}`;
+    console.log(chalk[index === selectedOptionIndex ? "cyan" : "white"](label));
   });
 };
 
 const handleKeyPress = (ch, key) => {
-  if (!key || !key.name) {
-    return;
-  }
+  if (!key || !key.name) return;
 
   switch (key.name) {
     case "up":
       selectedOptionIndex = Math.max(0, selectedOptionIndex - 1);
       break;
-
     case "down":
       selectedOptionIndex = Math.min(
         menuOptions.length - 1,
         selectedOptionIndex + 1
       );
       break;
-
     case "return":
       process.stdin.pause();
       handleReturnAction();
       break;
-
     case "c":
     case "z":
-      if (key.ctrl) {
-        // terminate key
-        process.exit(0);
-      }
+      if (key.ctrl) process.exit(0);
       break;
-
     default:
       return;
   }
@@ -101,14 +115,16 @@ const handleKeyPress = (ch, key) => {
 };
 
 const handleReturnAction = () => {
-  if (menuOptions[selectedOptionIndex].custom) {
+  const selectedOption = menuOptions[selectedOptionIndex];
+
+  if (selectedOption.custom) {
     handleCustomTimeInput();
-  } else if (menuOptions[selectedOptionIndex].cancel) {
+  } else if (selectedOption.cancel) {
     cancelShutdownSchedule();
-  } else if (menuOptions[selectedOptionIndex].exit) {
+  } else if (selectedOption.exit) {
     process.exit(0);
   } else {
-    shutdown(menuOptions[selectedOptionIndex].time);
+    shutdown(selectedOption.time);
   }
 };
 
@@ -137,18 +153,16 @@ const handleCustomTimeInput = () => {
   });
 };
 
-// Set up event listeners
 process.stdin.on("keypress", handleKeyPress);
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
-// Handle exit
 const handleExit = () => {
   console.log("\nExiting the shutdown program.");
   process.exit(0);
 };
-process.on("SIGINT", handleExit); // Ctrl+C
-process.on("SIGTSTP", handleExit); // Ctrl+Z
 
-// Display initial menu
+process.on("SIGINT", handleExit);
+process.on("SIGTSTP", handleExit);
+
 displayMenu();
